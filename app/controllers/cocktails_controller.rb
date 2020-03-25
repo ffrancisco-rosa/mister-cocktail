fdd64fc11b40bfb670c64ee0c7f2b7191add512c
Francisco Rosa <chicorosa@gmail.com>
2020-03-25 17:14:22 +0000

    added cloudinary, dotenv, basic landingpage and new controller action

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,5 +1,36 @@
 class CocktailsController < ApplicationController
+  before_action :find, only: [:show, :destroy]
   def index
     @cocktails = Cocktail.all
+  end
+
+  def show
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save!
+      redirect_to cocktail_path(@cocktail)
+    else
+      render new_cocktail_path, notice: "Something went wrong! Please try again!"
+    end
+  end
+
+  def destroy
+    @cocktail.destroy!
+  end
+
+  private
+
+  def find
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name, :photo)
   end
 end
